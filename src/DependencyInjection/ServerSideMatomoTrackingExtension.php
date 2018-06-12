db360217fe533b62069e8136d573d0805952cb03
Christian Fasching <christian.fasching@elements.at>
2018-06-12 11:43:09 +0200

    cleanup

--- src/DependencyInjection/ServerSideMatomoTrackingExtension.php
+++ src/DependencyInjection/ServerSideMatomoTrackingExtension.php
@@ -32,9 +32,6 @@ class ServerSideMatomoTrackingExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
